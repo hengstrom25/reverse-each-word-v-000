70b7cdc40ebed98ab274dd7468263144060e65f1
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-21 19:45:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reverse_array = sentence_array.each do | words |
     puts "#{words.reverse!}"
-    reverse_array.join
+    return reverse_array.join
   end
 end
 
