04acc63c7939d79ae551c895d5d2e4635a3e86b8
Heidi Engstrom <hengstrom25@gmail.com>
2018-04-21 19:28:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reverse_array = sentence_array.each do | words |
     "#{words.reverse!}"
+    "reverse_array.join"
   end
 end
 
